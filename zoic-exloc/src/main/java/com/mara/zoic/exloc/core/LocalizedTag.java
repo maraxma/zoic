ebd9e417112776da19ac55307f172db1c34096af
Mara.X.Ma <maraxma@hotmail.com>
2024-06-13 12:11:46 +0800

    support ResourceBundle.Control changing, now subclasses of LocalizedTag is able to use properties files

--- zoic-exloc/src/main/java/com/mara/zoic/exloc/core/LocalizedTag.java
+++ zoic-exloc/src/main/java/com/mara/zoic/exloc/core/LocalizedTag.java
@@ -7,7 +7,8 @@ import java.util.ResourceBundle;
 
 /**
  * 代表一个本地化字符串的唯一代码标识。
- * <p>一般来说此接口应该配合枚举来使用，使枚举实现此接口，然后使用单个枚举项来获取本地化字符串。</p>
+ * <p>一般来说此接口应该配合枚举来使用，使枚举实现此接口，然后使用单个枚举项来获取本地化字符串（推荐）。</p>
+ * <p>此接口也可以作为工具来直接获取本地化字符串（便捷使用，在结构化开发中更推荐前面的使用方式）。</p>
  * <p>此接口可以作为函数式接口来使用。</p>
  * @author MARA
  * @since 1.0.0 2022-01-13
@@ -16,6 +17,17 @@ import java.util.ResourceBundle;
 public interface LocalizedTag {
 
     ResourceBundle.Control CONTROL = new XmlResourceBundleControl();
+
+    /**
+     * 获得此接口实例使用的 {@link ResourceBundle.Control} 对象。
+     * <p>默认的实现会使用 {@link XmlResourceBundleControl}。此 Control 支持XML文件，相对于 properties 文件，
+     * XML文件虽然体积稍大但是排版清晰，支持UTF8编码，对于开发来说很方便。可以重写此方法让 {@link LocalizedTag} 使用自定义的 {@link ResourceBundle.Control}。</p>
+     * <p>{@link ResourceBundle} 本身提供了支持properties文件的 {@link ResourceBundle.Control}。</p>
+     * @return {@link ResourceBundle.Control} 对象
+     */
+    default ResourceBundle.Control getResourceBundleControl() {
+        return CONTROL;
+    }
 
     /**
      * 获得此对象的Tag，该Tag代表的是在本地化过程中使用的唯一标识。
@@ -38,7 +50,7 @@ public interface LocalizedTag {
 
         String localeFamily = detailedTag[0].trim();
         String key = detailedTag[1].trim();
-        String originalString = ResourceBundle.getBundle(localeFamily, locale, CONTROL).getString(key);
+        String originalString = ResourceBundle.getBundle(localeFamily, locale, getResourceBundleControl()).getString(key);
 
         Object[] finalReplacements = new Object[replacements.length];
         for (int i = 0; i < replacements.length; i++) {
@@ -56,7 +68,7 @@ public interface LocalizedTag {
     /**
      * 自动获取locale，返回此Tag所对应的本地化字符串。
      * <p>Locale是通过Spring提供的 {@link org.springframework.context.i18n.LocaleContextHolder LocalContextHolder} 来获取的。
-     * 在非Spring环境下无效。</p>
+     * 在非Spring环境下无效，请勿在非Spring环境下使用此方法，否则达不到预期的效果且如果没有spring-context依赖的话会抛出异常。</p>
      * @param replacements 占位符替换物（String.format）
      * @see org.springframework.context.i18n.LocaleContextHolder
      */
@@ -65,7 +77,7 @@ public interface LocalizedTag {
     }
 
     static LocalizedTag of(String localeFamily, String tag) {
-        if (localeFamily == null || tag == null || "".equals(localeFamily) || "".equals(tag)) {
+        if (localeFamily == null || tag == null || localeFamily.isEmpty() || tag.isEmpty()) {
             throw new IllegalArgumentException("localeFamily and tag cannot be null or empty");
         }
         return new StringLocalizedTag(localeFamily, tag);
--- zoic-exloc/src/test/java/com/mara/zoic/exloc/core/I18nTest.java
+++ zoic-exloc/src/test/java/com/mara/zoic/exloc/core/I18nTest.java
@@ -27,6 +27,33 @@ public class I18nTest {
         Assertions.assertEquals(rb_zh_cn, lt_new_zh_cn);
     }
 
+    @Test
+    void testI18n2() {
+        ResourceBundle.Control bundleControl = ResourceBundle.Control.getControl(ResourceBundle.Control.FORMAT_DEFAULT);
+        ResourceBundle resourceBundle = ResourceBundle.getBundle("i18n/testI18n_properties_bundle", Locale.SIMPLIFIED_CHINESE, bundleControl);
+        String rb_zh_cn = resourceBundle.getString("I0001");
+
+        String lt_enum_zh_cn = TestI18nCodes2.I0001.getLocalizedString(Locale.SIMPLIFIED_CHINESE);
+
+        Assertions.assertEquals(rb_zh_cn, lt_enum_zh_cn);
+
+        class MyStringLocalizedTag extends LocalizedTag.StringLocalizedTag {
+
+            @Override
+            public ResourceBundle.Control getResourceBundleControl() {
+                return ResourceBundle.Control.getControl(ResourceBundle.Control.FORMAT_DEFAULT);
+            }
+
+            public MyStringLocalizedTag(String localeFamily, String tag) {
+                super(localeFamily, tag);
+            }
+        }
+
+        String lt_new_zh_cn = new MyStringLocalizedTag("i18n/testI18n_properties_bundle", "I0001").getLocalizedString(Locale.SIMPLIFIED_CHINESE);
+
+        Assertions.assertEquals(rb_zh_cn, lt_new_zh_cn);
+    }
+
     enum TestI18nCodes implements LocalizedTag {
 
         I0001;
@@ -34,6 +61,21 @@ public class I18nTest {
         @Override
         public String tag() {
             return "i18n/testI18n" + ":" + name();
+        }
+    }
+
+    enum TestI18nCodes2 implements LocalizedTag {
+        I0001;
+
+        @Override
+        public ResourceBundle.Control getResourceBundleControl() {
+            // 使用自带的Control，此Control支持properties文件
+            return ResourceBundle.Control.getControl(ResourceBundle.Control.FORMAT_DEFAULT);
+        }
+
+        @Override
+        public String tag() {
+            return "i18n/testI18n_properties_bundle" + ":" + name();
         }
     }
 }
